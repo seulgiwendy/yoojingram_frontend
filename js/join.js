2976f97f9a3acc78c5b4400f33a2b047f05c805e
wheejuni <wheejuni@gmail.com>
2017-09-15 12:50:34 +0900

    회원가입 짧은 비밀번호 경고창 구현

--- js/join.js
+++ js/join.js
@@ -1,4 +1,53 @@
-function setJoinButtonActive() {
+var idCheck = true;
+var pwdCheck = false;
 
+function setJoinButtonActive() {
+    if (idCheck && pwdCheck == true){
+
+        $('#join-submit').attr('disabled', false);
+        console.log('submit button freed!');
+    }
+    else {
+        $('#join-submit').attr('disabled', true);
+        console.log('not yet!');
+
+    }
 
 }
+
+function checkId(){
+
+    var input = $('#id').val();
+    console.log(input);
+    setJoinButtonActive();
+
+}
+
+function checkPwd() {
+
+    var input = $('#pwd').val();
+
+
+    if (input.length > 5){
+        pwdCheck = true;
+        $('#pwd-warning').hide();
+    }
+
+    else if (input.length == 0){
+        pwdCheck = false;
+        $('#pwd-warning').hide();
+    }
+
+    else{
+        pwdCheck = false;
+        $('#pwd-warning').show();
+    }
+
+    setJoinButtonActive();
+
+}
+
+
+
+
+
